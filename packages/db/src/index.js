0ffbd8759011cd50a5734d592a6b013d09c9b409
Rashi Bhojwani <rashi.bhojwani16@gmail.com>
2026-07-17 17:03:27 +0530

    .

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -4,7 +4,7 @@ import { loadDotEnv } from './env.js';
 loadDotEnv();
 
 const { Pool } = pg;
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = "postgresql://reconciliation:Rashib123@reconciliation-db.cvi6iwyo0o2r.ap-south-1.rds.amazonaws.com:5432/postgres";
 export const databaseUrlConfigured = Boolean(databaseUrl && databaseUrl !== 'HEHE');
 export const pool = new Pool({
   connectionString: databaseUrlConfigured ? databaseUrl : undefined,
